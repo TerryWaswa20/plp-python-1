d3c3dd78e5aee6f70c1a2481eccc57fde00f340e
TerryWaswa20 <terrywaswa20@gmail.com>
2024-03-02 11:08:47 -0800

    PLP Python Assignment 1

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input('Enter Your Name: ')
+age = input('Enter Your Age: ')
+location = input('Your Location: ')
+print("Hello"+" "+ name +" " "You are"+ " "+ age +" " "years old, from"+" "+ location)
